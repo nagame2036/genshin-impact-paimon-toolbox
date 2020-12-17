a2f08b3cd1164df30db417970b732afaa2cfac8e
nagame2036 <nagame2036@163.com>
2020-12-18 06:39:28 +0800

    refactor: component style
    - extract common style of mat-form-field

--- src/app/character-stat/character-stat.module.ts
+++ src/app/character-stat/character-stat.module.ts
@@ -14,6 +14,7 @@ import {MatSelectModule} from '@angular/material/select';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatTabsModule} from '@angular/material/tabs';
 import {MatButtonModule} from '@angular/material/button';
+import {FlexModule} from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import {MatButtonModule} from '@angular/material/button';
     MatSelectModule,
     MatDividerModule,
     MatTabsModule,
-    MatButtonModule
+    MatButtonModule,
+    FlexModule
   ]
 })
 export class CharacterStatModule {
