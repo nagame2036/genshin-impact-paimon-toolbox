5514ccd78277d533f23a2f9f9953518964b18387
nagame2036 <nagame2036@163.com>
2021-01-06 13:55:33 +0800

    style(plan): minor style refactor

--- src/app/plan/plan.module.ts
+++ src/app/plan/plan.module.ts
@@ -1,10 +1,9 @@
 import {NgModule} from '@angular/core';
-import {SharedModule} from '../shared/shared.module';
 
+import {SharedModule} from '../shared/shared.module';
 import {PlanRoutingModule} from './plan-routing.module';
 import {CharacterAndGearModule} from '../character-and-gear/character-and-gear.module';
 import {MaterialModule} from '../material/material.module';
-
 
 @NgModule({
   declarations: [],
