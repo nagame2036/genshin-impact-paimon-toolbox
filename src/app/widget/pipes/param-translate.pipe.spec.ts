74c06b9e5998b560f6330dd8141a0b110056ca94
nagame2036 <nagame2036@163.com>
2021-03-27 09:53:44 +0800

    test(widget): test create ParamTranslatePipe

--- src/app/widget/pipes/param-translate.pipe.spec.ts
+++ src/app/widget/pipes/param-translate.pipe.spec.ts
@@ -1,26 +1,22 @@
 import {ParamTranslatePipe} from './param-translate.pipe';
-import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {inject, TestBed} from '@angular/core/testing';
 import {AppTestingModule} from '../../app-testing.module';
 import {WidgetModule} from '../widget.module';
+import {TranslateService} from '@ngx-translate/core';
 
 describe('ParamTranslatePipe', () => {
-  let pipe: ParamTranslatePipe;
-  let fixture: ComponentFixture<ParamTranslatePipe>;
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ParamTranslatePipe],
       imports: [WidgetModule, AppTestingModule],
-    }).compileComponents();
+    });
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ParamTranslatePipe);
-    pipe = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('create an instance', () => {
-    expect(pipe).toBeTruthy();
-  });
+  it('create an instance', inject(
+    [TranslateService],
+    (translator: TranslateService) => {
+      const pipe = new ParamTranslatePipe(translator);
+      expect(pipe).toBeTruthy();
+    },
+  ));
 });
