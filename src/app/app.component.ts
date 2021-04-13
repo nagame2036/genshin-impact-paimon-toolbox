9d1892a60ef79d7a2256db89bfb70178a5cf380d
nagame2036 <nagame2036@163.com>
2021-04-13 19:24:06 +0800

    feat(Layout): add website footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {I18n} from './widget/models/i18n.model';
 
 @Component({
   selector: 'app-root',
@@ -6,12 +7,25 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  i18n = I18n.create('core');
+
   routers = [
     {path: 'home'},
     {path: 'characters'},
     {path: 'weapons'},
+    {path: 'artifacts'},
     {path: 'inventory'},
     {path: 'resin'},
     {path: 'settings'},
   ];
+
+  showMenu = false;
+
+  year!: string;
+
+  constructor() {
+    const startYear = 2021;
+    const currYear = new Date().getFullYear();
+    this.year = currYear === startYear ? `${startYear}` : `${startYear}-${currYear}`;
+  }
 }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,11 +2,10 @@ import {NgModule} from '@angular/core';
 
 import {WidgetModule} from '../widget/widget.module';
 import {HeaderComponent} from './layout/header/header.component';
-import {SidenavComponent} from './layout/sidenav/sidenav.component';
 
 @NgModule({
-  declarations: [HeaderComponent, SidenavComponent],
-  exports: [HeaderComponent, SidenavComponent],
+  declarations: [HeaderComponent],
+  exports: [HeaderComponent],
   imports: [WidgetModule],
 })
 export class CoreModule {}
