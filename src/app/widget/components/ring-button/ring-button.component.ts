c88e856f9b11407b7127bb406725ad348b88ed73
nagame2036 <nagame2036@163.com>
2021-04-26 03:55:26 +0800

    refactor(Widget): minify ring-button.component

--- src/app/widget/components/ring-button/ring-button.component.ts
+++ src/app/widget/components/ring-button/ring-button.component.ts
@@ -1,15 +1,13 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'app-ring-button',
   templateUrl: './ring-button.component.html',
   styleUrls: ['./ring-button.component.scss'],
 })
-export class RingButtonComponent implements OnInit {
+export class RingButtonComponent {
   @Input()
   color: 'normal' | 'primary' | 'red' = 'normal';
-
-  colorClass = 'ring-btn-normal';
 
   @Input()
   disabled = false;
@@ -21,20 +19,6 @@ export class RingButtonComponent implements OnInit {
   clicked = new EventEmitter();
 
   constructor() {}
-
-  ngOnInit(): void {
-    switch (this.color) {
-      case 'primary':
-        this.colorClass = 'ring-btn-primary';
-        break;
-      case 'red':
-        this.colorClass = 'ring-btn-red';
-        break;
-      default:
-        this.colorClass = 'ring-btn-normal';
-        break;
-    }
-  }
 
   emitClick(): void {
     if (!this.disabled) {
